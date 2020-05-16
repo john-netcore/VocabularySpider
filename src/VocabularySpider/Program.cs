6eee919f0edfd3ffd97ca8cc281ff64e2c7e3c02
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-05-16 13:30:06 +0200

    First commit.

---
+++ src/VocabularySpider/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace VocabularySpider
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
