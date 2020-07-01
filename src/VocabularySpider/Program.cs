337534d08400751d7e81550bc4969f2f5d8ff1fc
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-01 12:11:43 +0200

    Retrieval of italian verb names.

--- src/VocabularySpider/Program.cs
+++ src/VocabularySpider/Program.cs
@@ -1,10 +1,31 @@
-﻿namespace VocabularySpider
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace VocabularySpider
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string[] indexes = File.ReadAllLines("./data/commonVerbIndexes.txt");
 
+            List<string> italianVerbNames = RetrieveVerbNames("italian", indexes);
+
+            System.Console.WriteLine("Finished");
+        }
+
+        static List<string> RetrieveVerbNames(string language, string[] indexes)
+        {
+            List<string> verbNames = new List<string>();
+            foreach (var index in indexes)
+            {
+                IEnumerable<(string VerbName, string ConjugationPath)> verbsInfo = ReversoContextCommonVerbs.RetrieveVerbsFromIndex("italian", index);
+                foreach (var verbInfo in verbsInfo)
+                {
+                    verbNames.Add(verbInfo.VerbName);
+                }
+            }
+            return verbNames;
         }
     }
 }
