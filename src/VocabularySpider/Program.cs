d24f19d403dd35df7261029e1bff71135bc315e6
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-01 10:21:34 +0200

    Added text file with common verb indexes.

--- src/VocabularySpider/Program.cs
+++ src/VocabularySpider/Program.cs
@@ -4,17 +4,7 @@
     {
         static void Main(string[] args)
         {
-            var englishPopularVerbs = new ReversoContextPopularVerbs("italian");
 
-            foreach (var value in englishPopularVerbs.RetrieveVerbs())
-            {
-                System.Console.WriteLine(value);
-            }
-
-            foreach (var value in englishPopularVerbs.RetrieveVerbTensesUrls())
-            {
-                System.Console.WriteLine(value);
-            }
         }
     }
 }
