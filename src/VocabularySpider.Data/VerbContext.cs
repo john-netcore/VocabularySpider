f19f5e54651e9f3f745c553741962deaf9e39baa
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-07 16:16:18 +0200

    Added DbContext configuration.

--- src/VocabularySpider.Data/VerbContext.cs
+++ src/VocabularySpider.Data/VerbContext.cs
@@ -1,8 +1,27 @@
 ﻿using System;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json.Linq;
+using VocabularySpider.BL;
 
 namespace VocabularySpider.Data
 {
-    public class VerbContext
+    public class VerbContext : DbContext
     {
+        public DbSet<Verb> Verbs { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder builder)
+        {
+            builder.UseSqlServer(GetConnectionString());
+        }
+
+        private static string GetConnectionString()
+        {
+            var jsonString = File.ReadAllText("./data/secrets.json");
+            var myObj = JObject.Parse(jsonString);
+            var connString = myObj.SelectToken("connectionString").Value<string>();
+            System.Console.WriteLine(connString);
+            return connString;
+        }
     }
 }
