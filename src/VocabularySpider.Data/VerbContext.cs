3d2842e1373af5aa96839f4202bc1bdd8265480e
John Lundgren <john.lundgren.lexicon@gmail.com>
2020-07-08 09:53:56 +0200

    DbSet and fuent api implementation.

--- src/VocabularySpider.Data/VerbContext.cs
+++ src/VocabularySpider.Data/VerbContext.cs
@@ -9,10 +9,30 @@ namespace VocabularySpider.Data
     public class VerbContext : DbContext
     {
         public DbSet<Verb> Verbs { get; set; }
+        public DbSet<VerbTense> VerbTenses { get; set; }
+        public DbSet<Conjugation> Conjugations { get; set; }
+        public DbSet<SimpleConjugation> SimpleConjugations { get; set; }
+        public DbSet<CompoundConjugation> CompoundConjugations { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder builder)
         {
+            System.Console.WriteLine("On configuring invoked...");
             builder.UseSqlServer(GetConnectionString());
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            System.Console.WriteLine("On model creating invoked...");
+            builder.Entity<Verb>().Property(b => b.Language).IsRequired();
+            builder.Entity<Verb>().Property(b => b.Infinitive).IsRequired();
+
+            builder.Entity<VerbTense>().Property(b => b.TenseName).IsRequired();
+
+            builder.Entity<Conjugation>().Property(b => b.InflictedVerb).IsRequired();
+
+            builder.Entity<SimpleConjugation>().Property(b => b.Pronoun).IsRequired();
+
+            builder.Entity<CompoundConjugation>().Property(b => b.AuxiliaryVerb).IsRequired();
         }
 
         private static string GetConnectionString()
@@ -20,7 +40,6 @@ namespace VocabularySpider.Data
             var jsonString = File.ReadAllText("./data/secrets.json");
             var myObj = JObject.Parse(jsonString);
             var connString = myObj.SelectToken("connectionString").Value<string>();
-            System.Console.WriteLine(connString);
             return connString;
         }
     }
